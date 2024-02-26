fb9fc7fa005c861a51d97b27e8304591fa0cfd27
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2024-02-26 18:25:16 +0530

    contact form frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/submitContactForm', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, message }),
+      });
+
+      const data = await response.json();
+      toast("Successfully submitted form");
+      console.log(data);
+
+      // You can handle the response here and provide feedback to the user
+    } catch (error) {
+      console.error('Error submitting contact form:', error);
+    }
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <form onSubmit={handleSubmit}>
+        <label for="exampleFormControlInput1" className="form-label">Name</label>
+        <input
+          type="name"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)} /><br />
+
+        <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          placeholder="enter your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}></textarea><br />
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <ToastContainer />
+
+
     </div>
   );
 }
